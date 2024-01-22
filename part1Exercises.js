39e724336577da0a341e49b97e38269e689bb7d9
Kevin Cordero <77799855+kevcorca@users.noreply.github.com>
2024-01-22 11:06:09 -0600

    Add files via upload

---
+++ part1Exercises.js
@@ -0,0 +1,29 @@
+// 1a
+alert('Good morning!');
+
+// 1b
+alert('Kevin');
+
+// 1c
+10 + 5;
+
+// 1d
+20 - 5;
+
+// 1e
+2 + 2 - 5;
+
+// 1f
+document.innerHTML = 'Good morning!';
+
+// 1g
+document.innerHTML = 'Kevin';
+
+// 1h
+alert('The total amount of the order is ' + (10 + 8 + 20));
+
+// 1i
+alert('The total amount of money in your account is ' + (100 - 20 - 50 + 200));
+
+// 1j
+document.innerHTML = '';
