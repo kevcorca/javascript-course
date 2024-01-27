fcbd881c2680434b4e95896d4bf2d676bcc1c11d
Kevin Cordero <77799855+kevcorca@users.noreply.github.com>
2024-01-26 18:41:46 -0600

    Add files via upload

---
+++ part3Exercises.js
@@ -0,0 +1,45 @@
+// 3a
+'My name is: ';
+
+// 3b
+'Kevin';
+
+// 3c
+'My name is: ' + 'Kevin';
+
+// 3d
+'Total cost: $' + (5 + 3);
+
+// 3e
+`Total cost: $${5+3}`;
+
+// 3f
+alert(`Total cost: $${5+3}`);
+
+// 3g
+'Total cost: $' + (599 + 295) / 100;
+
+// 3h
+`Total cost: $${(599 + 295) / 100}`;
+
+// 3i
+alert(`Total cost: $${(599 + 295) / 100}`);
+
+// 3j
+alert('Total cost: $' + (599 + 295) / 100 + '\nThank you, come again!');
+
+// 3ja
+alert(`Total cost: $${(599 + 295) / 100}
+Thank you, come again!`);
+
+// 3k
+`Items(${2 + 2}): $${(2095 * 2 + 799 * 2) / 100}`;
+
+// 3l
+'Shipping & handling: $' + (499 + 499) / 100;
+
+// 3m
+'Total before tax: $' + (2095 * 2 + 799 * 2 + 499 * 2) / 100;
+
+// 3n
+'Estimated tax (10%): $' + Math.round((2095 * 2 + 799 * 2 + 499 * 2) * 0.1) /100;
